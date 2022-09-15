39be2e1c59b44a5bc7bc0a0cd539fed2aa744da4
Esteban Boada Castillo <estebanboada1994@gmail.com>
2022-09-15 10:58:08 -0500

    Avance scripts sql.
    Solicitud a base de datos. tablas ujuy y palermo.

---
+++ dev_esteban/dags/scripts/uni_palermo.sql
@@ -0,0 +1,12 @@
+SELECT 
+"universidad", "careers",
+"fecha_de_inscripcion",
+"names",
+"sexo",
+EXTRACT(YEAR FROM age(CAST("birth_dates" as DATE))) as "age",
+"codigo_postal", 
+"correos_electronicos"
+FROM palermo_tres_de_febrero
+WHERE CAST("fecha_de_inscripcion" AS DATE)
+BETWEEN TO_DATE('01/Sep/20','DD/Mon/YY')
+    AND TO_DATE('01/Feb/21','DD/Mon/YY');

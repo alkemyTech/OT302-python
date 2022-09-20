30070f882028467293d5be86a73ec042b7674d1a
anresz73 <andresreisz@gmail.com>
2022-09-20 00:13:36 -0300

    OT302-40 - Add Path in logger.py

--- dev_ar/dags/functions/logger.py
+++ dev_ar/dags/functions/logger.py
@@ -1,43 +1,38 @@
 # Modules
 import logging
-#import os
-
-#print(os.path.relpath(__file__))
+from pathlib import Path
 
 # Functions
 def logger(
     logger_name = 'test',
     logger_format = '%(asctime)s - %(name)s - %(message)s',
     logger_datefmt = '%Y-%m-%d',
-    logger_file_path = './logs/logs.txt'
+    logger_file_path = 'logs',
+    logger_file_name = 'logs'
 ):
     """
     Create and return logger using loggin module with parameters passed
     Set to INFO level
-    Use Example: any_logger = logger(logger_name = 'any logger') // any_logger.info('Message')
+    Use Example: any_logger = logger(logger_name = 'any logger', logger_file_name = 'any_logs') // any_logger.info('Message')
     Args:
         logger_name (str, optional): Logger name. Defaults to 'test'.
         logger_format (str, optional): Log format. Defaults to '%(asctime)s - %(name)s - %(message)s'.
         logger_datefmt (str, optional): Format time in datetime format. Defaults to '%Y-%m-%d'.
-        logger_file_path (str, optional): Log file name and relative path. Defaults to './logs/logs.txt'.
-
+        logger_file_path (str, optional): Log file name and relative path. Defaults to 'logs'.
+        logger_file_name (str, optional): relative path name. Defaults to 'logs'
     Returns:
-        (logging.logger) : logging logger object
+        (logging.logger) : configured logging logger object
     """
-    # logging.basicConfig(
-    #     filename = logger_file_path,
-    #     format = logger_format,
-    #     datefmt = logger_datefmt,
-    #     level = logging.INFO,
-    # )
-
     # Create Logger
     custom_logger = logging.getLogger(logger_name)
     # Set Level
     custom_logger.setLevel(logging.INFO)
+    # Check if dir exists 
+    if not Path(logger_file_path).exists():
+        Path(logger_file_path).mkdir(parents = False, exist_ok = False)
     # Create File Handler
     # Check also StreamHandler() with sys.stdout
-    file_handler = logging.FileHandler(logger_file_path)
+    file_handler = logging.FileHandler(Path(logger_file_path, logger_file_name).with_suffix('.txt'))
     # Create Formatter and add It to Handler
     custom_formatter = logging.Formatter(
         fmt = logger_format,

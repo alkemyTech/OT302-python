f56b3beb51ef3828303350ecec622ebb0b95f843
anresz73 <andresreisz@gmail.com>
2022-10-07 17:18:25 -0300

    OT302-112 Add functions chunkify, chunking_express, shuffle_express, chunkerizer and chunkerizer_from_root in mapreduce.py

--- dev_ar/bigdata/mapreduce.py
+++ dev_ar/bigdata/mapreduce.py
@@ -6,7 +6,9 @@ import re
 import numpy as np
 from pathlib import Path
 from datetime import datetime
-from operator import itemgetter
+from operator import itemgetter, add
+from functools import reduce
+from itertools import islice
 from bigdata_logger import bigdata_logger
 
 # Functions
@@ -24,12 +26,28 @@ def mapreduce_tasker(
         map_root (function): task map function
         reduce (function): task reduce function
     """
+    return mapreduce_tasker_from_root(
+        xml_root = get_single_xml(xml_file_path = xml_file_path),
+        map_root = map_root,
+        reduce = reduce
+        )
+
+def mapreduce_tasker_from_root(
+    xml_root,
+    map_root,
+    reduce
+    ):
+    """
+    Get functions as params
+    Args:
+        xml_file_path (int): xml file path
+        map_root (function): task map function
+        reduce (function): task reduce function
+    """
     return reduce(
         map_root(
             map_function(
-                get_single_xml(
-                    xml_file_path = xml_file_path
-                    )
+                xml_root
                 )
             )
         )
@@ -118,6 +136,14 @@ def mapper_task_3(
             aux_tuple = (xml_item['Id'], aux_delta.days)
             deltas.append(aux_tuple)
     return deltas
+
+def mapper_task_3_(mapped_xml):
+    def _f(item):
+        if item.get('PostTypeId') == '1':
+            aux_delta = datetime.fromisoformat(item['LastActivityDate']) - datetime.fromisoformat(item['CreationDate'])
+            aux_tuple = (item['Id'], aux_delta.days)
+            return aux_tuple
+    return list(filter(lambda x: x is not None, map(_f, mapped_xml)))
 
 def reducer_task_3(
     mapped_tuples
@@ -224,3 +250,82 @@ def _get_tags(string):
     if string is None:
         return None
     return re.findall(r'<(.+?)>', string)
+
+# Chunkify Functions
+def chunkify(
+    iterable,
+    chunk_size
+    ):
+    """
+    Function that chunks an iterable into parts of chunk size
+    Args:
+        iterable (iter): Iterable form of iterator, list, generator, etc-
+        chunk_size (int): size of the chunk
+    Yields:
+        iter: iterable of same type as entered iterable with chunk_size
+    """
+    for e in range(0, len(iterable), chunk_size):
+        yield iterable[e: e + chunk_size]
+
+def chunking_express(
+    items,
+    chunk_size
+    ):
+    """
+    Function that chunks an iterable into parts of chunk size
+    Args:
+        items (iter): Iterable form of iterator, list, generator, etc
+        chunk_size (int): size of the chunk
+    Yields:
+        iter: iterable of same type as entered iterable with chunk_size
+    """
+    iterator = iter(items)
+    while chunk := tuple(islice(iterator, chunk_size)):
+        yield chunk
+
+def shuffle_express(chunks):
+    """
+    Function that reduce tuples made in mapping stage using reduce from functools and add from operator built in modules
+    Args:
+        chunks (iterable): iterable with all mapped and proceced chunks in form of tuples
+
+    Returns:
+        iterable of tuples: full tuple list or iterable
+    """
+    return reduce(add, chunks)
+
+def chunkerizer(xml_file_path, mapper_task, reducer_task, N = 50):
+    """
+    Function that takes xml file path, extract data from it, map and reduce it with custom functions as params
+    Args:
+        xml_file_path (str): file path
+        mapper_task (function): function that performs the mapping
+        reducer_task (function): function that performs the reducing
+        N (int, optional): Number of chunks. Defaults to 50.
+
+    Returns:
+        Depends on the reducing function what is returned
+    """
+    # Get root from file path
+    xml_root = get_single_xml(xml_file_path)
+    # Use function below
+    return chunkerizer_from_root(xml_root = xml_root, mapper_task = mapper_task, reducer_task = reducer_task, N = N )
+
+def chunkerizer_from_root(xml_root, mapper_task, reducer_task, N = 50):
+    """
+    Function that takes xml root, extract data from it, map and reduce it with custom functions as params
+    Args:
+        xml_file_path (root object): root from xml.etree.ElementTree getroot()
+        mapper_task (function): function that performs the mapping
+        reducer_task (function): function that performs the reducing
+        N (int, optional): Number of chunks. Defaults to 50.
+
+    Returns:
+        Depends on the reducing function what is returned
+    """
+    # n length of each chunk from N number of chunks
+    n = len(xml_root) // N
+    chunked_xml = chunking_express(xml_root, n)
+    # Map each chunk, shuffle and reduce it
+    chunked_map = map(lambda x: mapper_task(map_function(x)), chunked_xml)
+    return reducer_task(shuffle_express(chunked_map))
